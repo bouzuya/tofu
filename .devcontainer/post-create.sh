50204775a7450f16821f5665d26e08e8709e36d3
bouzuya <m@bouzuya.net>
2026-08-04 22:31:49 +0900

    Add topcoat-cli

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,3 +7,4 @@ echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 rustup component add --toolchain nightly rustfmt
 rustup component add llvm-tools-preview
 cargo install cargo-llvm-cov
+cargo install topcoat-cli
