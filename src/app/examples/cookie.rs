4bf5d36d67c683016a7bad75ad77fe41009f1c9c
bouzuya <m@bouzuya.net>
2026-08-13 23:14:17 +0900

    Add cookie store example

--- src/app/examples/cookie.rs
+++ src/app/examples/cookie.rs
@@ -11,6 +11,11 @@ async fn page(cx: &::topcoat::context::Cx) -> ::topcoat::Result {
     );
     jar.add(::topcoat::cookie::cookie! { "bar" = "baz"; Path = "/"; HttpOnly; SameSite = Lax });
 
+    let store = my_cookie_store(cx);
+    store
+        .update(|it| it.items.push("foo".to_string()))
+        .commit()?;
+
     ::topcoat::view::view! {
         <h1>"Cookie"</h1>
         <p>"Check the cookie in the browser devtools."</p>
@@ -25,4 +30,25 @@ fn my_signed_cookies(cx: &::topcoat::context::Cx) -> impl ::topcoat::cookie::Coo
         .default_http_only(true)
         .default_same_site(::topcoat::cookie::SameSite::Lax)
         .default_path("/")
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct MyCookieStoreValue {
+    items: Vec<String>,
+}
+
+fn my_cookie_store(
+    cx: &::topcoat::context::Cx,
+) -> ::topcoat::cookie::CookieStore<MyCookieStoreValue, impl ::topcoat::cookie::Cookies> {
+    use topcoat::cookie::Cookies as _;
+
+    ::topcoat::cookie::cookie_store::<MyCookieStoreValue, _>(
+        ::topcoat::cookie::signed_cookies(cx)
+            .default_secure(false)
+            .default_http_only(true)
+            .default_same_site(::topcoat::cookie::SameSite::Lax)
+            .default_path("/"),
+        "my_store",
+    )
+    .parse_or(MyCookieStoreValue { items: vec![] })
 }
