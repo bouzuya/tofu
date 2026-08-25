83b2eac270818b760f4eeff0b6ce7ae0f414c3f5
bouzuya <m@bouzuya.net>
2026-08-25 20:54:12 +0900

    Fix cli version

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,4 +7,4 @@ echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 rustup component add --toolchain nightly rustfmt
 rustup component add llvm-tools-preview
 cargo install cargo-llvm-cov
-cargo install topcoat-cli
+cargo install topcoat-cli@0.5.0 --locked
